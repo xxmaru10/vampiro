b32148c4992a1f5581f8a73888838ad9da41116e
xxmaru10 <xxmaru10@gmail.com>
2026-04-27 09:29:40 -0300

    f2

---
+++ blinkmotion-app/vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './', // Garante que caminhos de assets sejam relativos ao index.html
+})
